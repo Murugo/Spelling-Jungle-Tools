58fbb23afe5fb89fdd4d4595262df9078c4eb95e
Murugo <murugalstudio@gmail.com>
2026-04-27 00:34:31 -0700

    Add ability to show alert messages

--- winfauxs/main.js
+++ winfauxs/main.js
@@ -1,12 +1,23 @@
 const WINDOW_BORDERS_BEFORE = ['nw', 'n', 'ne', 'w'];
 const WINDOW_BORDERS_AFTER = ['e', 'sw', 's', 'se'];
 
-const titleButton = {
+const titleButtonEnum = {
   NONE: 0,
   MINIMIZE: 1,
   MAXIMIZE: 2,
   CLOSE: 4,
 };
+
+const alertTypeEnum = {
+  INFO: 0,
+  WARNING: 1,
+  ERROR: 2,
+};
+
+const alertButtonEnum = {
+  OK: 0,
+  CANCEL: 1,
+}
 
 let windows = [];
 let desktopIcons = [];
@@ -14,20 +25,25 @@ let activeWindow;
 let activeIcon;
 
 class ViewBorder {
-  constructor(viewWindow, parentElement) {
+  constructor(viewWindow, parentElement, resizable) {
     this.viewWindow = viewWindow;
     this.parentElement = parentElement;
+    this.resizable = resizable;
     this.setUp();
   }
 
   setUp() {
     this.borderElements = [];
     const addBorderElement = (element, border) => {
-      element.addEventListener('mousedown', (event) => {
-        this.viewWindow.setAsActive();
-        this.viewWindow.startResize(event.clientX, event.clientY, event.buttons, [...border]);
-        document.getElementsByTagName('body')[0].style.cursor = `${border}-resize`;
-      });
+      if (this.resizable) {
+        element.addEventListener('mousedown', (event) => {
+          this.viewWindow.setAsActive();
+          this.viewWindow.startResize(event.clientX, event.clientY, event.buttons, [...border]);
+          document.getElementsByTagName('body')[0].style.cursor = `${border}-resize`;
+        });
+      } else {
+        element.style.cursor = 'default';
+      }
       this.borderElements.push(element);
     };
     const contentElement = this.parentElement.querySelector('.view-window-inner');
@@ -53,32 +69,37 @@ class ViewBorder {
 }
 
 class ViewWindow {
-  constructor(id, startOpened = true, titleButtons = titleButton.MINIMIZE | titleButton.MAXIMIZE | titleButton.CLOSE) {
-    this.id = id;
+  constructor(element,
+              startOpened = true,
+              resizable = true,
+              deleteOnClose = false,
+              titleButtons = titleButtonEnum.MINIMIZE | titleButtonEnum.MAXIMIZE | titleButtonEnum.CLOSE) {
+    this.element = element;
     this.dragging = false;
     this.dragStartX = 0;
     this.dragStartY = 0;
+    this.resizable = resizable;
     this.resizingLeft = false;
     this.resizingRight = false;
     this.resizingTop = false;
     this.resizingBottom = false;
     this.anchorX = 0;
     this.anchorY = 0;
+    this.deleteOnClose = deleteOnClose;
     this.maximized = false;
     this.setUp(startOpened, titleButtons);
   }
 
   setUp(startOpened, titleButtons) {
-    this.element = document.getElementById(this.id);
     this.contentElement = this.element.querySelector('.view-window-content');
 
     this.createTitle(titleButtons);
-    this.viewBorder = new ViewBorder(this, this.element);
+    this.viewBorder = new ViewBorder(this, this.element, this.resizable);
     this.createEvents();
     if (startOpened) {
-      this.show(/*opened=*/true);
+      this.open();
     } else {
-      this.hide(/*closed=*/true);
+      this.close();
     }
   }
 
@@ -93,17 +114,17 @@ class ViewWindow {
     this.titleElement.appendChild(buttonsContainer);
     this.element.querySelector('.view-window-inner').prepend(this.titleElement);
 
-    if (titleButtons & titleButton.MINIMIZE) {
+    if (titleButtons & titleButtonEnum.MINIMIZE) {
       this.minimizeButtonElement = document.createElement('button');
       this.minimizeButtonElement.className = 'view-window-minimize';
       buttonsContainer.appendChild(this.minimizeButtonElement);
     }
-    if (titleButtons & titleButton.MAXIMIZE) {
+    if (titleButtons & titleButtonEnum.MAXIMIZE) {
       this.maximizeButtonElement = document.createElement('button');
       this.maximizeButtonElement.className = 'view-window-maximize';
       buttonsContainer.appendChild(this.maximizeButtonElement);
     }
-    if (titleButtons & titleButton.CLOSE) {
+    if (titleButtons & titleButtonEnum.CLOSE) {
       this.closeButtonElement = document.createElement('button');
       this.closeButtonElement.className = 'view-window-close';
       buttonsContainer.appendChild(this.closeButtonElement);
@@ -122,9 +143,9 @@ class ViewWindow {
     this.taskbarElement.style.backgroundImage = this.iconUrl;
     this.taskbarElement.addEventListener('mousedown', (event) => {
       if (activeWindow !== this) {
-        this.show(/*opened=*/false);
+        this.show();
       } else {
-        this.hide(/*closed=*/false);
+        this.hide();
       }
     });
     document.getElementById('taskbarWindows').appendChild(this.taskbarElement);
@@ -142,7 +163,7 @@ class ViewWindow {
     });
     if (this.minimizeButtonElement) {
       this.minimizeButtonElement.addEventListener('click', () => {
-        this.hide(/*closed=*/false);
+        this.hide();
       });
     }
     if (this.maximizeButtonElement) {
@@ -156,7 +177,7 @@ class ViewWindow {
     }
     if (this.closeButtonElement) {
       this.closeButtonElement.addEventListener('click', () => {
-        this.hide(/*closed=*/true);
+        this.close();
       });
     }
     this.element.addEventListener('mousedown', (event) => {
@@ -287,21 +308,33 @@ class ViewWindow {
     this.element.style.zIndex = priority;
   }
 
-  hide(closed) {
+  hide() {
     this.element.style.display = 'none';
-    if (closed && this.taskbarElement) {
-      document.getElementById('taskbarWindows').removeChild(this.taskbarElement);
-      this.taskbarElement = undefined;
-    }
     this.setNotActive();
   }
 
-  show(opened) {
+  close() {
+    this.hide();
+    if (this.taskbarElement) {
+      document.getElementById('taskbarWindows').removeChild(this.taskbarElement);
+      this.taskbarElement = undefined;
+    }
+    if (this.deleteOnClose) {
+      windows.splice(windows.indexOf(this), 1);
+      document.body.removeChild(this.element);
+    }
+  }
+
+  show() {
     this.element.style.removeProperty('display');
-    if (opened && !this.taskbarElement) {
+    this.setAsActive();
+  }
+
+  open() {
+    this.show();
+    if (!this.taskbarElement) {
       this.addToTaskbar();
     }
-    this.setAsActive();
   }
 }
 
@@ -319,7 +352,7 @@ class Icon {
     this.element = document.getElementById(this.id);
 
     this.element.addEventListener('dblclick', () => {
-      this.viewWindow.show(/*opened=*/true);
+      this.viewWindow.open();
       this.deselect();
     });
     this.element.addEventListener('mousedown', (event) => {
@@ -371,12 +404,89 @@ class Icon {
   }
 }
 
+function alert(title, message, alertType, buttons = [alertButtonEnum.OK]) {
+  const windowElement = document.createElement('div');
+  windowElement.className = 'view-window';
+  windowElement.style.setProperty('--title', title);
+  windowElement.style.height = 'auto';
+  windowElement.style.minHeight = 0;
+
+  const innerElement = document.createElement('div');
+  innerElement.className = 'view-window-inner';
+  windowElement.appendChild(innerElement);
+
+  const contentElement = document.createElement('div');
+  contentElement.className = 'view-window-content';
+  innerElement.appendChild(contentElement);
+
+  const alertElement = document.createElement('div');
+  alertElement.className = 'view-window-alert';
+  contentElement.appendChild(alertElement);
+
+  const messageElement = document.createElement('div');
+  messageElement.className = 'view-window-alert-message';
+  alertElement.appendChild(messageElement);
+
+  const iconElement = document.createElement('div');
+  iconElement.className = 'view-window-alert-message-icon';
+  switch (alertType) {
+    case alertTypeEnum.INFO:
+      iconElement.setAttribute('state', 'info');
+      break;
+    case alertTypeEnum.WARNING:
+      iconElement.setAttribute('state', 'warning');
+      break;
+    case alertTypeEnum.ERROR:
+      iconElement.setAttribute('state', 'error');
+      break;
+  }
+  messageElement.appendChild(iconElement);
+
+  const textElement = document.createElement('div');
+  textElement.className = 'view-window-alert-message-text';
+  textElement.innerHTML = message;
+  messageElement.appendChild(textElement);
+
+  document.body.prepend(windowElement);
+  
+  const viewWindow = new ViewWindow(
+    windowElement,
+    /*startOpened=*/true,
+    /*resizable=*/false,
+    /*deleteOnClose=*/true,
+    titleButtonEnum.NONE);
+  windows.push(viewWindow);
+
+  const buttonsElement = document.createElement('div');
+  buttonsElement.className = 'view-window-alert-buttons';
+  alertElement.appendChild(buttonsElement);
+
+  buttons.forEach((buttonType) => {
+    const buttonElement = document.createElement('button');
+    switch (buttonType) {
+      case alertButtonEnum.OK:
+        buttonElement.innerHTML = "OK";
+        buttonElement.addEventListener('click', () => {
+          viewWindow.close();
+        })
+        break;
+    };
+    buttonsElement.append(buttonElement);
+  });
+
+  const rect = windowElement.getBoundingClientRect();
+  windowElement.style.setProperty('--xpos', (window.innerWidth - rect.width) / 2);
+  windowElement.style.setProperty('--ypos', (window.innerHeight - rect.height) / 2);
+}
+
 function onLoad() {
-  windows.push(new ViewWindow('mainWindow', /*startOpened=*/false));
-  windows.push(new ViewWindow('secondaryWindow', /*startOpened=*/false));
+  windows.push(new ViewWindow(document.querySelector('#mainWindow'), /*startOpened=*/false));
+  windows.push(new ViewWindow(document.querySelector('#secondaryWindow'), /*startOpened=*/false));
 
   desktopIcons.push(new Icon('mainWindowIcon', windows[0]));
   desktopIcons.push(new Icon('secondaryWindowIcon', windows[1]));
+
+  alert('Welcome', '<3', alertTypeEnum.INFO);
 
   document.addEventListener('mousedown', (event) => {
     if (event.buttons !== 1) return;
